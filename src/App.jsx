d9b1931980bb4e20c004f2f9c05225a6df144747
osmanerdemvural <84151504+osmanerdemvural@users.noreply.github.com>
2025-01-27 00:25:48 -0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
  /* app.jsx - Osman Erdem Vural - Student ID 301399745 - 23/01/2025 */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 //import { ThemeProvider } from '@material-ui/styles';
-import MainRouter from '../MainRouter';
 //import Contact from './contact'
 //import theme from '../theme';
 //import { hot } from 'react-hot-loader'
-import { Divider } from '@mui/material';
 const App = () => {
  return (
- <Router>
- 
- <MainRouter />
-
- </Router>
+    <HashRouter>
+    <App />
+  </HashRouter>
  );
 };
 export default App;
